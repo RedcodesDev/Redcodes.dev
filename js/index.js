12c93282146eff47f8fbc4eaec76724796c9a71f
David <david.kesselmann@gmail.com>
2021-05-17 12:43:34 +0200

    Upper right Buttons working

--- js/index.js
+++ js/index.js
@@ -6,3 +6,16 @@ const navMenu = document.querySelector('#nav-links');
 burgerIcon.addEventListener('click', () => {
     navMenu.classList.toggle('is-active');
 });
+
+//Button New Tab Handler
+
+const githubButton = document.querySelector('#github-button');
+const discordButton = document.querySelector('#discord-button');
+
+githubButton.addEventListener('click', () => {
+    window.open("https://github.com/RedstonecraftHD", "_blank");
+});
+
+discordButton.addEventListener('click', () => {
+    window.open("https://statifybot.net/discord/", "_blank");
+});
